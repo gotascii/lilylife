a040148b66239f15e8b9b6ee6d347073d276bb45
Justin Marney <gotascii@gmail.com>
2009-04-09 23:58:26 -0400

    wrapped up life started implementing color

--- forplus.js
+++ forplus.js
@@ -4,16 +4,21 @@ function $forplus (max) {
   this.max = max;
 
 	this.inlet1 = new this.inletClass("inlet1", this, "bang starts counter");
+	this.inlet2 = new this.inletClass("inlet2", this, "count");
 
 	this.outlet1 = new this.outletClass("outlet1", this, "count");
 	this.outlet2 = new this.outletClass("outlet2", this, "bang when done");
 
 	this.inlet1["bang"] = function () {
-	  for(var i = 0; i < max; i++) {
+	  for(var i = 0; i < thisPtr.max; i++) {
 		  thisPtr.outlet1.doOutlet(i);
 	  }
 		thisPtr.outlet2.doOutlet("bang");
 	}
+
+	this.inlet1["anything"] = function (max) {
+	  thisPtr.max = parseInt(max);
+  }
 
 	return this;
 }
